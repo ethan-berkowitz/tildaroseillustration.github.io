180ed7ca9908dfb08c2a8717e0d6cead04917c4a
Ethan Berkowitz <ethanberkowitz@Ethans-iMac.home>
2025-10-03 17:19:36 +0300

    changed tilda titles

--- scripts/index_navbar.js
+++ scripts/index_navbar.js
@@ -12,22 +12,6 @@ function highlightActiveSection() {
 	const sectionMap = {
 		"/index.html": "featured",
 		"/": "featured",
-		
-		// Textile pages
-		"/textile/fiber_art.html": "textile",
-		"/textile/patterns.html": "textile",
-		"/projects/fiber_art_autumn.html": "textile",
-		"/projects/fiber_art_countryside.html": "textile",
-		"/projects/fiber_art_flounder.html": "textile",
-		"/projects/fiber_art_spring_moss.html": "textile",
-		"/projects/fiber_art_summer_moss.html": "textile",
-		"/projects/fiber_art_sunshine.html": "textile",
-		"/projects/fiber_art_waterfall.html": "textile",
-		"/projects/fiber_art_wave.html": "textile",
-		
-		// About pages
-		"/about/bio.html": "about",
-		"/about/contact.html": "about"
 	};
 
 	const path = window.location.pathname;
